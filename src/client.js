d17b26d174dc2d15d25e4959a6b8b5a47af991f9
Cain Atkinson <yellowsink@riseup.net>
2022-05-31 12:11:45 +0100

    better grid

--- src/client.js
+++ src/client.js
@@ -8,7 +8,8 @@ function animateCell(x, y) {
   elem.doTransition({
     state: {
       "background-color": "#ec2a6a",
-      transform: "translateY(-20px) scaleY(.5) rotate(45deg)",
+      transform: "translateY(-20px)",
+      "pointer-events": "none"
     },
     ms: 500,
     easing: EASE.ease,
@@ -17,8 +18,27 @@ function animateCell(x, y) {
   setTimeout(() => elem.doTransition({ reset: true }));
 }
 
-window.handleBgCellClick = (x, y) => {
-  console.log("rippling bg cell", x, y);
+// this may appear inefficient but it takes 9ms to run on my machine
+function calculateSquaredDistances(x, y) {
+  const cellsByDistance = new Map();
 
+  for (const cell of allTiles) {
+    if (cell[0] === x && cell[1] === y) continue;
+
+    const dist = (cell[0] - x) ** 2 + (cell[1] - y) ** 2;
+
+    if (cellsByDistance.has(dist))
+      cellsByDistance.get(dist).push(cell);
+    else cellsByDistance.set(dist, [cell]);
+  }
+
+  return cellsByDistance;
+}
+
+window.handleBgCellClick = (x, y) => {
   animateCell(x, y);
+  const sqDistances = calculateSquaredDistances(x, y);
+
+  for (const [dist, cells] of sqDistances)
+    setTimeout(() => cells.forEach(c => animateCell(...c)), dist * 3);
 };
